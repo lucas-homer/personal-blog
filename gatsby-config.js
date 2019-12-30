01140c5fd3d99714c7a01c7dca2eee01584f914f
Lucas Homer <lucashomer@lucass-mbp.home>
2019-12-29 17:07:20 -0800

    add favicon and gatsby-plugin-manifest and gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,23 @@ module.exports = {
       options: {},
     },
     "gatsby-plugin-theme-ui",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Lucas Homer`,
+        short_name: `Lucas`,
+        start_url: `/`,
+        background_color: `hsla(191, 74%, 98%, 1)`,
+        theme_color: `hsla(191, 74%, 15%, 1)`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    "gatsby-plugin-offline",
   ],
   // Customize your site metadata:
   siteMetadata: {
+    siteUrl: "https://lucashomer.com",
     title: `Lucas Homer`,
     author: `Lucas Homer`,
     description: `A place for thoughts and things.`,
